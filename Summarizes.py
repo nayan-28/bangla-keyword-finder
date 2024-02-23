57095d24f01fdf480d147901c8386702ac1a51a6
nayan-28 <nayanmalakar720@gmail.com>
2024-02-23 17:38:23 +0600

    train data

--- Summarizes.py
+++ Summarizes.py
@@ -22,8 +22,16 @@ stop_words = set(stopwords.words('bengali'))
 # Generate summary
 summary = generate_summary(bangla_text, stop_words)
 
+# List of key words to extract
+keywords_to_extract = ["জ্বর", "সর্দি", "কাশি", "ব্যাথা", "পায়ে ব্যাথা", "মাথা ব্যাথা", "মাথা যন্ত্রণা", "মাথা ঝিমঝিম"]
+
+# Extract keywords from the summary
+extracted_keywords = [keyword for keyword in keywords_to_extract if keyword in summary]
+
 # Print the summary
-print("\nOriginal Text:")
+print("\nআপনার প্রদানকৃত তথ্য:")
 print(bangla_text)
-print("\nSummary:")
+print("\nসারাংশ:")
 print(summary)
+print("\nআপনার সমস্যাগুলো:")
+print(extracted_keywords)
